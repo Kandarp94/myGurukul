4f6eb7af2f013273a63babcc788dacf0a5d9580d
Kandarp <kandarpsharma94skt@gmail.com>
2017-07-16 02:04:14 +0530

    07-16-2017

--- app/src/main/java/com/example/kandarp/mygurukul/Home.java
+++ app/src/main/java/com/example/kandarp/mygurukul/Home.java
@@ -6,10 +6,16 @@ package com.example.kandarp.mygurukul;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class Home extends AppCompatActivity {
@@ -17,6 +23,9 @@ public class Home extends AppCompatActivity {
     GridView gridView;
     private SessionManager session;
     static final String[] TITLE = new String[]{"My Profile", "Performance Details", "Institutional Holidays", "Time Table"};
+    private DrawerLayout mdrawerlayout;
+    private ActionBarDrawerToggle mtoggle;
+    private Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +34,22 @@ public class Home extends AppCompatActivity {
 
         session = new SessionManager(Home.this);
 
+        mtoolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mtoolbar);
+
+        mdrawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mtoggle = new ActionBarDrawerToggle(this, mdrawerlayout, R.string.open, R.string.close);
+
+        mdrawerlayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         final ViewFlipper myView = (ViewFlipper) findViewById(R.id.viewF);
         myView.setFlipInterval(2000);
         myView.startFlipping();
+
 
         gridView = (GridView) findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(Home.this, TITLE));
@@ -39,7 +61,7 @@ public class Home extends AppCompatActivity {
                         startActivity(intent);
                         break;
                     }
-                    case 1:{
+                    case 1: {
                         Intent intent = new Intent(Home.this, PerformanceDetails.class);
                         startActivity(intent);
                         break;
@@ -54,5 +76,30 @@ public class Home extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                session.logoutUser();
+                finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
